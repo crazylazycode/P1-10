e8fc82a924955202420985a1a874d578cfcfe331
crazylazy <crazytobeawake@gmail.com>
2020-11-03 15:22:52 +0530

    Indian Stae lookup added

--- P6-QuickAutocompleteApp/js/main.js
+++ P6-QuickAutocompleteApp/js/main.js
@@ -7,13 +7,13 @@ search.addEventListener('input', () => searchStates(search.value));
 // Search states in json file and filter it
 
 const searchStates = async searchText => {
-    const res = await fetch('../docs/states.json');
+    const res = await fetch('../docs/in.json');
     const states = await res.json();
 
     //Get matches to current input
     let matches = states.filter(state => {
         const regex = new RegExp(`^${searchText}`,'gi');
-        return state.name.match(regex) || state.abbr.match(regex);
+        return state.admin.match(regex) || state.city.match(regex);
     });
 
     if(searchText.length === 0) {
@@ -28,9 +28,9 @@ const outputHtml = matches => {
     if(matches.length > 0){
        const html = matches.map(match => `
             <div class="card card-body">
-            <h4>${match.name} (${match.abbr}) <span class="text-primary">${match.capital}</span>
+            <h4>${match.city} (${match.admin}) <span class="text-primary">${match.country}</span>
             </h4>
-            <small>Lat: ${match.lat} / Long: ${match.long}</small>
+            <small>Lat: ${match.lat} / Long: ${match.lng}</small>
             </div>
        `).join('');
        matchList.innerHTML = html;
